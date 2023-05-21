41c59ab6dcf834e7efe781600c63b94a33dfa9a7
raissa campos <raissa.campos26@gmail.com>
2023-05-21 11:53:37 +0100

    feat: new product shelf component

---
+++ components/product/NewProductShelf.tsx
@@ -0,0 +1,87 @@
+import ProductCard from "$store/components/product/ProductCard.tsx";
+import SliderJS from "$store/islands/NewSliderJS.tsx";
+import Icon from "$store/components/ui/Icon.tsx";
+import Slider from "$store/components/ui/new-slider/Slider.tsx";
+import type { ItemsPerPage } from "$store/components/ui/new-slider/Slider.tsx";
+import { SendEventOnLoad } from "$store/sdk/analytics.tsx";
+import { useId } from "preact/hooks";
+import { mapProductToAnalyticsItem } from "deco-sites/std/commerce/utils/productToAnalyticsItem.ts";
+import { useOffer } from "$store/sdk/useOffer.ts";
+import type { LoaderReturnType } from "$live/types.ts";
+import type { Product } from "deco-sites/std/commerce/types.ts";
+
+export interface Props {
+  title: string;
+  products: LoaderReturnType<Product[] | null>;
+  itemsPerPage: ItemsPerPage;
+  infinite?: boolean;
+}
+
+function ProductShelf({
+  title,
+  infinite,
+  products,
+  itemsPerPage = { sm: 2, md: 3, lg: 4, default: 1 },
+}: Props) {
+  const id = useId();
+
+  if (!products || products.length === 0) {
+    return null;
+  }
+
+  return (
+    <div
+      id={id}
+      class="container grid grid-cols-[48px_1fr_48px] grid-rows-[48px_1fr_48px_1fr] py-10 px-0 sm:px-5"
+    >
+      <h2 class="text-center row-start-1 col-span-full">
+        <span class="font-medium text-2xl">{title}</span>
+      </h2>
+
+      <Slider
+        infinite={infinite}
+        class="carousel carousel-center sm:carousel-end col-span-full row-start-2 row-end-5"
+      >
+        {products?.map((product, index) => (
+          <Slider.Item
+            itemsPerPage={itemsPerPage}
+            index={index}
+            class="carousel-item first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0 [&>*]:mx-3"
+          >
+            <ProductCard product={product} itemListName={title} />
+          </Slider.Item>
+        ))}
+      </Slider>
+
+      <>
+        <div class="hidden relative sm:block z-10 col-start-1 row-start-3">
+          <Slider.PrevButton class="btn btn-circle btn-outline absolute right-1/2 bg-base-100">
+            <Icon size={20} id="ChevronLeft" strokeWidth={3} />
+          </Slider.PrevButton>
+        </div>
+        <div class="hidden relative sm:block z-10 col-start-3 row-start-3">
+          <Slider.NextButton class="btn btn-circle btn-outline absolute left-1/2 bg-base-100">
+            <Icon size={20} id="ChevronRight" strokeWidth={3} />
+          </Slider.NextButton>
+        </div>
+      </>
+      <SliderJS rootId={id} infinite={infinite} />
+      <SendEventOnLoad
+        event={{
+          name: "view_item_list",
+          params: {
+            item_list_name: title,
+            items: products.map((product) =>
+              mapProductToAnalyticsItem({
+                product,
+                ...(useOffer(product.offers)),
+              })
+            ),
+          },
+        }}
+      />
+    </div>
+  );
+}
+
+export default ProductShelf;
